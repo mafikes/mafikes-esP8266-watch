abf9b82cdd417bad1d10f8114e39e81c09835f99
Martin Antoš <mafikes@gmail.com>
2021-01-24 21:22:25 +0100

    add Show Text App controlled from web admin

---
+++ src/ShowTextApp.cpp
@@ -0,0 +1,48 @@
+#include <ShowTextApp.h>
+#include <Config.h>
+#include <Display.h>
+#include <Colors.h>
+#include <WString.h>
+
+#define MATRIX_WIDTH 32;
+unsigned long previousAnimation = 0;
+
+ShowTextApp::ShowTextApp(String _showText, bool useDefaultFont, int speed, DisplayColor color)
+{
+    defaultFont = useDefaultFont;
+    textColor = color;
+    animationSpeed = speed;
+    showText = _showText;
+    
+    if(defaultFont) {
+        showTextLenght = _showText.length() * 7;
+    } else {
+        showTextLenght = _showText.length() * 6;
+    }
+
+    position = MATRIX_WIDTH;
+}
+
+void ShowTextApp::beforeRender()
+{
+    
+}
+
+void ShowTextApp::render(Display& display) 
+{   
+    unsigned long currentAnimation = millis();
+
+    if(currentAnimation - previousAnimation >= animationSpeed) {
+        if(position == -showTextLenght) {
+            position = MATRIX_WIDTH;
+        }
+
+        // Serial.println(position);
+
+        display.clear();
+        display.drawText(showText, defaultFont, {position, 0}, textColor);
+        position--;
+
+        previousAnimation = currentAnimation; 
+    }
+}
