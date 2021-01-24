f74766df27585474d1fc01682495a3bdea82e279
Martin Antoš <mafikes@gmail.com>
2021-01-24 21:21:54 +0100

    add server read local file web html

--- src/Wifi.cpp
+++ src/Wifi.cpp
@@ -1,22 +1,29 @@
 #include <Wifi.h>
 #include <Display.h>
+#include "Settings.h"
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncWiFiManager.h>
+#include "LittleFS.h"
 #include <ESP8266mDNS.h>
-#include <WiFiManager.h>
+#include "ArduinoJson.h"
 
-// https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WebServer
-ESP8266WebServer server(80);
+#include <ApplicationManager.h>
 
-void handleRoot() {
-    server.send(200, "text/plain", "Hello from esp8266!\r\n");
-}
+AsyncWebServer server(80);
+DNSServer dns;
 
 void Wifi::setup() {
-    Serial.println(F("WiFi Setup")); 
-    WiFiManager wifiManager;
+    if(!LittleFS.begin()) {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+
+    Serial.println(F("WiFi Setup"));
+    AsyncWiFiManager wifiManager(&server,&dns);
     wifiManager.setTimeout(180); 
 
     if(!wifiManager.autoConnect("MAFIKES WATCH")) { 
@@ -26,19 +33,74 @@ void Wifi::setup() {
         ESP.restart(); //reset and try again 
     } 
 
-    ipAddress = WiFi.localIP().toString();
-    
-    Serial.println(F("WiFi connected...")); 
-    Serial.println(F("Use this URL to connect: http://")); Serial.println(ipAddress+"/");
+    IP_ADDRESS = WiFi.localIP().toString();
 
-    server.on("/", handleRoot);
-    
+    Serial.println(F("WiFi connected...")); 
+    Serial.println(F("Use this URL to connect: http://")); Serial.println(IP_ADDRESS+"/");
+
+    // Homepage
+    server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+
+    server.on("/info", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/plain", String(ESP.getFreeHeap()));
+    });
+        
+    // API
+    server.on("/change-view", HTTP_GET, [](AsyncWebServerRequest *request){
+        if(request->hasParam("button")) {
+            String button = request->getParam("button")->value();
+
+            if(button == "1") {
+                ApplicationManager::getInstance().btn1_process();
+            } else if (button == "2") {
+                ApplicationManager::getInstance().btn2_process();
+            } else if (button == "3") {
+                ApplicationManager::getInstance().btn3_process();
+            }
+        }
+
+        request->send(200, "text/plain", "OK");
+    });
+
+    server.on("/brightness", HTTP_GET, [](AsyncWebServerRequest *request){
+        if(request->hasParam("value")) {
+            String value = request->getParam("value")->value();
+            ApplicationManager::getInstance().setBrightness(value.toInt());
+        }
+
+        request->send(200, "text/plain", "OK");
+    });
+
+    server.on("/watch-color", HTTP_GET, [](AsyncWebServerRequest *request){
+        // TODO: set color watch
+        request->send(200, "text/plain", "OK");
+    });
+
+    server.on("/show-text", HTTP_GET, [](AsyncWebServerRequest *request){
+        if(request->hasParam("value")) {
+            String text = request->getParam("value")->value();
+            if(text) {
+                ApplicationManager::getInstance().showText(text);
+            } 
+        }
+
+        request->send(200, "text/plain", "OK");
+    });
+
+    server.on("/change-weather-api", HTTP_GET, [](AsyncWebServerRequest *request){
+        if(request->hasParam("value")) {
+            String value = request->getParam("value")->value();
+            // TODO: save weather api        
+        }
+
+        request->send(200, "text/plain", "OK");
+    });
+
     server.begin();
-    Serial.println("HTTP server started");
+    Serial.println("HTTP Web server running.");
 }
 
 void Wifi::loop() {
-    server.handleClient();
-    MDNS.update();
+
 }
 
