ade851817e4121ba3f8f3ad3ddd6923e95156599
Martin Antoš <mafikes@gmail.com>
2021-01-28 19:56:48 +0100

    add loading reset add show icon

--- include/Display.h
+++ include/Display.h
@@ -17,7 +17,8 @@ class Display {
         uint32_t color(DisplayColor);
 
         int lastShowedIcon = 0;
-        int iconAnimationRepeated = 0;
+        int lastIconAnimationCount = 0;
+        int loadingRepeated = 0;
         int repeatedAnimation = 0;
 
         void setup();
@@ -34,8 +35,10 @@ class Display {
         void drawBitmap(unsigned char[], DisplayPosition, DisplayColor, int16_t, int16_t);
         void fixdrawRGBBitmap(int16_t, int16_t, const uint32_t*, int16_t, int16_t);
         void showTextWithIconAnimated(const uint32_t[][64], int, String, DisplayPosition, DisplayColor);
-        void showLoading(int);
-        void showAnimateIcon(const uint32_t[][64], int, int = 4, int = 300);
+        void resetLoading();
+        void showLoading(int = 300);
+        void showAnimateIcon(const uint32_t[][64], int, int = 4, int = 200);
+        void showIcon(const uint32_t[][64]);
         void drawTextWithIcon(String, DisplayPosition, DisplayColor = {255, 255, 255});
         void showLogo();
         void show();
