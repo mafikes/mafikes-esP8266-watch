f85a16aa0ce6c90f53a27f91628c30441fef11af
Martin Antoš <mafikes@gmail.com>
2021-10-15 00:53:51 +0200

    add new theme

--- src/Apps/TimeApp.cpp
+++ src/Apps/TimeApp.cpp
@@ -1,6 +1,7 @@
 #include <TimeApp.h>
 #include <Config.h>
 #include <RTC.h>
+#include <Icons.h>
 
 String TimeApp::repairDigit(int digit) 
 {
@@ -20,20 +21,22 @@ DisplayColor TimeApp::getColor()
     DisplayColor watchColor = COLOR_WHITE;
 
     if(clockColorActive == 1) {
-      watchColor = COLOR_RED;
+        watchColor = COLOR_RED;
     } else if(clockColorActive == 2) {
-      watchColor = COLOR_YELLOW;
+        watchColor = COLOR_YELLOW;
     } else if(clockColorActive == 3) {
-      watchColor = COLOR_ORANGE;
+        watchColor = COLOR_ORANGE;
     } else if(clockColorActive == 4) {
-      watchColor = COLOR_BLUE;      
-    } else if(clockColorActive == 4) {
-      watchColor = COLOR_GREEN;
+        watchColor = COLOR_BLUE;      
+    } else if(clockColorActive == 4) {        
+        watchColor = COLOR_GREEN;
+    } else if(clockColorActive == 5) {
+        watchColor = COLOR_GRAY;
     } else {
-      clockColorActive = 0;
-      watchColor = COLOR_WHITE;
+        clockColorActive = 0;
+        watchColor = COLOR_WHITE;
     }
-
+    
     return watchColor;
 }
 
@@ -51,7 +54,27 @@ void TimeApp::render(Display& display)
     if(clockTheme == 1) {        
         display.drawText(repairDigit(now.hour()) + ":" + repairDigit(now.minute()) + ":" + repairDigit(now.second()), false, {2, 0}, clockColor);
     } else if(clockTheme == 2) {       
-        display.drawText(repairDigit(now.hour()) + ":" + repairDigit(now.minute()), false, {8, 0}, clockColor);        
+        display.drawText(repairDigit(now.hour()) + ":" + repairDigit(now.minute()), false, {8, 0}, clockColor);      
+    } else if(clockTheme == 3) {
+        DisplayColor headerColor = clockColor;
+
+        if(headerColor.red == COLOR_WHITE.red && headerColor.blue == COLOR_WHITE.blue && headerColor.green == COLOR_WHITE.green) {
+            headerColor = COLOR_GRAY;
+        }
+
+        display.drawLine(0, 0, 8, 0, headerColor);
+        display.drawLine(0, 1, 8, 1, headerColor);
+        display.drawLine(0, 2, 8, 2, COLOR_WHITE);
+        display.drawLine(0, 3, 8, 3, COLOR_WHITE);
+        display.drawLine(0, 4, 8, 4, COLOR_WHITE);
+        display.drawLine(0, 5, 8, 5, COLOR_WHITE);
+        display.drawLine(0, 6, 8, 6, COLOR_WHITE);
+        display.drawLine(0, 7, 8, 7, COLOR_WHITE);
+
+        int16_t positionDay = now.day() >= 10 ? 1 : 3;
+        display.drawText(String(now.day()), false, {positionDay, 1}, COLOR_BLACK);
+
+        display.drawText(repairDigit(now.hour()) + ":" + repairDigit(now.minute()), false, {12, 0}, clockColor);
     } else {
         display.drawText(repairDigit(now.hour()) + ":" + repairDigit(now.minute()), false, {8, 0}, clockColor);
 
@@ -59,9 +82,15 @@ void TimeApp::render(Display& display)
         int offset = 0;
         for(int day = 1; day <= 7; day++) {
             if(day == now.dayOfTheWeek()) {
-                display.drawLine(2 + offset, 7, 4 + offset, 7, clockColor);
+                DisplayColor colorLine = clockColor;
+
+                if(colorLine.red == COLOR_GRAY.red && colorLine.blue == COLOR_GRAY.blue && colorLine.green == COLOR_GRAY.green) {
+                    colorLine = COLOR_WHITE;
+                }
+
+                display.drawLine(2 + offset, 7, 4 + offset, 7, colorLine);
             } else {
-                display.drawLine(2 + offset, 7, 4 + offset, 7, {188, 188, 188});
+                display.drawLine(2 + offset, 7, 4 + offset, 7, COLOR_GRAY);
             }  
             offset += 4;      
         }
