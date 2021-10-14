7cde5adeb091e5b105b4424570a500afac482d5b
Martin Antoš <mafikes@gmail.com>
2021-10-15 01:07:37 +0200

    Update Colors.h

--- include/Colors.h
+++ include/Colors.h
@@ -8,3 +8,4 @@ const DisplayColor COLOR_YELLOW = {255, 255, 0};
 const DisplayColor COLOR_GREEN = {0, 128, 0}; 
 const DisplayColor COLOR_BLUE = {0, 0, 255}; 
 const DisplayColor COLOR_ORANGE = {255, 69, 0}; 
+const DisplayColor COLOR_GRAY ={188, 188, 188};
