d1d3b03d79a5eddcff30b1d35a5892011dd213d1
Martin Antoš <mafikes@gmail.com>
2021-10-19 00:16:08 +0200

    add load watch custom color

--- data/app.js
+++ data/app.js
@@ -47,6 +47,29 @@ function sendRequest(method, data, callback) {
  * Load data from device
  */
 function loadData() {
+    function rgbaToHex(rgba) {
+        var inParts = rgba.substring(rgba.indexOf("(")).split(","),
+            r = parseInt(trim(inParts[0].substring(1)), 10),
+            g = parseInt(trim(inParts[1]), 10),
+            b = parseInt(trim(inParts[2]), 10),
+            a = parseFloat(trim(inParts[3].substring(0, inParts[3].length - 1))).toFixed(2);
+        var outParts = [
+          r.toString(16),
+          g.toString(16),
+          b.toString(16),
+          Math.round(a * 255).toString(16).substring(0, 2)
+        ];
+      
+        // Pad single-digit output values
+        outParts.forEach(function (part, i) {
+          if (part.length === 1) {
+            outParts[i] = '0' + part;
+          }
+        })
+      
+        return ('#' + outParts.join(''));
+    }
+
     sendRequest('config.json', '', (response) => {
         let data = JSON.parse(response);
         console.log('Config: success load data.', data);       
@@ -59,6 +82,8 @@ function loadData() {
 
         _elements.weatherLocation.value = data.weather_location;
         _elements.weatherApi.value = data.weather_key;
+
+        _elements.customColorWatch.value = rgbaToHex(data.watch_color);
     });
 }
 
